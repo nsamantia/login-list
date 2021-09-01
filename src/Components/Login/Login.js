5891c4c45242a78ee251f70a8d945761c9b8e0ed
namantia1@gmail.com <namantia1@gmail.com>
2021-09-01 15:44:07 -0400

    text inputs setState for username and password

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,9 +1,27 @@
-import React, { useSate } from 'react';
+import React, { useState } from 'react';
 
 const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   return (
     <div>
-      <p>Login.js</p>
+      <div>
+        <input
+          type="text"
+          name="username"
+          placeholder="user@rapptrlabs.com"
+          onChange={(e) => setUsername(e.target.value)}
+        ></input>
+      </div>
+      <div>
+        <input
+          type="text"
+          name="password"
+          placeholder="Must be at least 4 characters"
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
+      </div>
     </div>
   );
 };
