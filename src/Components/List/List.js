6509d443b04aa25c25218d740b196f174a5576a9
namantia1@gmail.com <namantia1@gmail.com>
2021-09-01 16:55:43 -0400

    todo list mapped and rendered

--- src/Components/List/List.js
+++ src/Components/List/List.js
@@ -1,9 +1,23 @@
 import React, { useState } from 'react';
+import ListRender from '../ListRender/ListRender';
 
 const List = () => {
+  const [todo, setTodo] = useState(['walk dog', 'pet dog']);
   return (
     <div>
-      <p>List.js</p>
+      <div>
+        <input
+          type="text"
+          name="ToDo"
+          placeholder="Add an item to your list"
+        ></input>
+        <button>Save</button>
+      </div>
+      <div>
+        {todo.map((todo) => {
+          return <ListRender todo={todo} />;
+        })}
+      </div>
     </div>
   );
 };
---
+++ src/Components/ListRender/ListRender.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ListRender = (props) => {
+  return (
+    <div>
+      <div>
+        <p>{props.todo}</p>
+      </div>
+    </div>
+  );
+};
+
+export default ListRender;
