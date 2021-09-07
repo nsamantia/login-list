1e2bdd40ec31a6e76ee484dc94f8cc72586a0367
namantia1@gmail.com <namantia1@gmail.com>
2021-09-07 11:05:19 -0400

    added login button, header, and input labels

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -7,20 +7,33 @@ const Login = () => {
   return (
     <div>
       <div>
-        <input
-          type="text"
-          name="username"
-          placeholder="user@rapptrlabs.com"
-          onChange={(e) => setUsername(e.target.value)}
-        ></input>
+        <h1>Rapptr Labs</h1>
       </div>
       <div>
-        <input
-          type="text"
-          name="password"
-          placeholder="Must be at least 4 characters"
-          onChange={(e) => setPassword(e.target.value)}
-        ></input>
+        <form>
+          <label>
+            Username
+            <input
+              type="text"
+              name="username"
+              placeholder="user@rapptrlabs.com"
+              onChange={(e) => setUsername(e.target.value)}
+            ></input>
+          </label>
+
+          <label>
+            Password
+            <input
+              type="text"
+              name="password"
+              placeholder="Must be at least 4 characters"
+              onChange={(e) => setPassword(e.target.value)}
+            ></input>
+          </label>
+        </form>
+      </div>
+      <div>
+        <button>Login</button>
       </div>
     </div>
   );
